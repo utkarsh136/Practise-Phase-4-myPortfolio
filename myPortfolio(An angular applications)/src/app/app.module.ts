aa6b7b82f01e2fb9376761ff7c39f02abd1d4d58
utkarsh136 <81348131+utkarsh136@users.noreply.github.com>
2022-09-23 03:44:05 +0530

    myPortfolio application.
    1)This following application create a signup page.
    2)This following application create a sign in page.
    3)This following application has a landing page which create my portfolio.

---
+++ myPortfolio(An angular applications)/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    SignupComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
